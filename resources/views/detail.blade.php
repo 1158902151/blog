a2f8863c3a3a724b3fce009128e145e06ec14466
xieqiyong <1158902151@qq.com>
2019-03-20 11:45:55 +0800

    fix

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -2,8 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title><?php echo config('web.web_site');?></title>
+    <title><?php echo $title;?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/static/bootstrap/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/res/layui/css/layui.css">
     <link rel="stylesheet" href="/res/static/css/mian.css">
 </head>
@@ -24,50 +25,51 @@
                     <p><?php echo $detail->content;?></p>
                     <img src="<?php echo $detail->img_url;?>" alt="">
                     <div class="count layui-clear">
-                        <span class="pull-left">阅读 <em>100000+</em></span>
-                        <span class="pull-right like"><i class="layui-icon layui-icon-praise"></i><em>999</em></span>
+                        <span class="pull-left">阅读 <em><?php echo $detail->view;?></em></span>
                     </div>
                 </div>
             </div>
             <a name="comment"> </a>
             <div class="comt layui-clear">
-                <a href="javascript:;" class="pull-left">评论</a>
-                <a href="comment.html" class="pull-right">写评论</a>
+                {{--<a href="javascript:;" class="pull-left">评论</a>--}}
+                {{--<a href="comment.html" class="pull-right">写评论</a>--}}
             </div>
-            <div id="LAY-msg-box">
-                <div class="info-item">
-                    <img class="info-img" src="/res/static/images/info-img.png" alt="">
-                    <div class="info-text">
-                        <p class="title count">
-                            <span class="name">一片空白</span>
-                            <span class="info-img like"><i class="layui-icon layui-icon-praise"></i>5.8万</span>
-                        </p>
-                        <p class="info-intr">父爱如山，不善表达。回想十多年前，总记得父亲有个宽厚的肩膀，小小的自己跨坐在上面，越过人山人海去看更广阔的天空，那个时候期望自己有一双翅膀，能够像鸟儿一样飞得高，看得远。虽然父亲有时会和自己开玩笑，但在做错事的时候会受到严厉的训斥。父亲有双粗糙的大手掌。</p>
-                    </div>
-                </div>
-                <div class="info-item">
-                    <img class="info-img" src="/res/static/images/info-img.png" alt="">
-                    <div class="info-text">
-                        <p class="title count">
-                            <span class="name">一片空白</span>
-                            <span class="info-img like"><i class="layui-icon layui-icon-praise"></i>5.8万</span>
-                        </p>
-                        <p class="info-intr">父爱如山，不善表达。回想十多年前，总记得父亲有个宽厚的肩膀，小小的自己跨坐在上面，越过人山人海去看更广阔的天空，那个时候期望自己有一双翅膀，能够像鸟儿一样飞得高，看得远。虽然父亲有时会和自己开玩笑，但在做错事的时候会受到严厉的训斥。父亲有双粗糙的大手掌。</p>
-                    </div>
-                </div>
-            </div>
+            {{--<div id="LAY-msg-box">--}}
+                {{--<div class="info-item">--}}
+                    {{--<img class="info-img" src="/res/static/images/info-img.png" alt="">--}}
+                    {{--<div class="info-text">--}}
+                        {{--<p class="title count">--}}
+                            {{--<span class="name">一片空白</span>--}}
+                            {{--<span class="info-img like"><i class="layui-icon layui-icon-praise"></i>5.8万</span>--}}
+                        {{--</p>--}}
+                        {{--<p class="info-intr">父爱如山，不善表达。回想十多年前，总记得父亲有个宽厚的肩膀，小小的自己跨坐在上面，越过人山人海去看更广阔的天空，那个时候期望自己有一双翅膀，能够像鸟儿一样飞得高，看得远。虽然父亲有时会和自己开玩笑，但在做错事的时候会受到严厉的训斥。父亲有双粗糙的大手掌。</p>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+                {{--<div class="info-item">--}}
+                    {{--<img class="info-img" src="/res/static/images/info-img.png" alt="">--}}
+                    {{--<div class="info-text">--}}
+                        {{--<p class="title count">--}}
+                            {{--<span class="name">一片空白</span>--}}
+                            {{--<span class="info-img like"><i class="layui-icon layui-icon-praise"></i>5.8万</span>--}}
+                        {{--</p>--}}
+                        {{--<p class="info-intr">父爱如山，不善表达。回想十多年前，总记得父亲有个宽厚的肩膀，小小的自己跨坐在上面，越过人山人海去看更广阔的天空，那个时候期望自己有一双翅膀，能够像鸟儿一样飞得高，看得远。虽然父亲有时会和自己开玩笑，但在做错事的时候会受到严厉的训斥。父亲有双粗糙的大手掌。</p>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
         </div>
     </div>
 </div>
 <div class="footer">
     <p>
         <span>&copy; 2018</span>
-        <span><a href="http://www.layui.com" target="_blank">layui.com</a></span>
+        <span><a href="http://www.layui.com" target="_blank">或许期待一次绽放</a></span>
         <span>MIT license</span>
     </p>
     <p><span>人生就是一场修行</span></p>
 </div>
+<script type="text/javascript" src="{{ URL::asset('static/bootstrap/bootstrap.min.js') }}"></script>
 <script src="/res/layui/layui.js"></script>
+
 <script>
     layui.config({
         base: '/res/static/js/'
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,7 +38,7 @@
     <div class="footer">
         <p>
             <span>&copy; 2018</span>
-            <span><a href="http://www.layui.com" target="_blank">layui.com</a></span>
+            <span><a href="http://www.layui.com" target="_blank">或许期待一次绽放</a></span>
             <span>MIT license</span>
         </p>
         <p><span>人生就是一场修行</span></p>
